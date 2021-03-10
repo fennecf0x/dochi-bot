27e28d9720d90c564f1269584cfe1774fa39500b
fennecf0x <0x@ruu.kr>
2021-03-10 08:43:35 +0000

    add build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+git pull
+
+DOCKER_BUILDKIT=1 docker build -t dochi-bot-main-app "$(pwd)"/main-app
+
+docker rename dochi-bot-main-app dochi-bot-main-app-old
+
+docker run -d \
+    --name dochi-bot-main-app \
+    --mount type=bind,source="$(pwd)"/database,target=/app/database \
+    --mount type=bind,source="$(pwd)"/assets,target=/app/assets \
+    --env-file "$(pwd)"/main-app/.env \
+    dochi-bot-main-app
+
+docker rm -f dochi-bot-main-app-old
