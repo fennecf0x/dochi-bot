a24e1173d3985a9d8b1872ec65a1470a44af193f
Keonwoo Kim <keonwoo@kaist.ac.kr>
2021-04-06 16:29:02 +0900

    tt

---
+++ main-app/src/dochi/command/item/ff_triple_triad.py
@@ -0,0 +1,118 @@
+import discord
+from .item import CommandItem
+from ...state import state
+from ...games.ff_triple_triad import FFTripleTriad
+from .finance import ChangeFinance
+from ...database.types import CurrencyType
+
+class JoinFFTripleTriad(CommandItem):
+    async def __call__( # type: ignore
+        self,
+        client: discord.Client,
+        message: discord.Message,
+        **kwargs,
+    ):
+        """
+        join FF triple triad
+        """
+
+        if (False, "FFTripleTriad", message.author.id) not in state.games:
+            game = FFLottery(client, message.author.id)
+            state.games[game.id] = game
+
+
+class NotifyFFLottery(CommandItem):
+    async def __call__( # type: ignore
+        self,
+        client: discord.Client,
+        message: discord.Message,
+        **kwargs,
+    ):
+        """
+        notify next move in FF lottery
+        """
+
+        if (False, "FFLottery", message.author.id) not in state.games:
+            return {**kwargs, "is_satisfied": False}
+
+        game: FFLottery = state.games[(False, "FFLottery", message.author.id)]
+
+        if game.stage < 3:
+            return {**kwargs, "content": "A부터 I까지 중에서 하나를 골라줘!\n\n" + game.print_board()}
+
+        if game.stage == 3:
+            return {**kwargs, "content": "A부터 H까지 중에서 하나를 골라줘!\n\n" + game.print_board()}
+
+        if game.stage == 4:
+            points = game.get_points()
+            content = f"{points}원을 얻었어!\n\n" + game.print_board()
+            
+            await ChangeFinance(currency_type=CurrencyType.MONEY, amount=points, incremental=True)(client, message)
+            await game.prev_message.delete()
+
+            state.games.pop((False, "FFLottery", message.author.id), None)
+            return {**kwargs, "points": points, "content": content}
+
+
+class PlayFFLottery(CommandItem):
+    async def __call__( # type: ignore
+        self,
+        client: discord.Client,
+        message: discord.Message,
+        *,
+        move: str,
+        **kwargs,
+    ):
+        """
+        play a move in FF lottery
+        """
+
+        if (False, "FFLottery", message.author.id) not in state.games:
+            return {**kwargs, "is_satisfied": False}
+
+        game: FFLottery = state.games[(False, "FFLottery", message.author.id)]
+
+        return {**kwargs, "is_satisfied": game.play(None, move)}
+
+
+class StoreFFLotteryMessage(CommandItem):
+    async def __call__( # type: ignore
+        self,
+        client: discord.Client,
+        message: discord.Message,
+        *,
+        prev_message: discord.Message,
+        **kwargs,
+    ):
+        """
+        store sent message in FF lottery
+        """
+
+        if (False, "FFLottery", message.author.id) not in state.games:
+            return {**kwargs, "is_satisfied": False}
+
+        game: FFLottery = state.games[(False, "FFLottery", message.author.id)]
+        game.prev_message = prev_message
+
+        return kwargs
+
+
+class DeleteFFLotteryMessage(CommandItem):
+    async def __call__( # type: ignore
+        self,
+        client: discord.Client,
+        message: discord.Message,
+        **kwargs,
+    ):
+        """
+        delete sent message in FF lottery
+        """
+
+        if (False, "FFLottery", message.author.id) not in state.games:
+            return {**kwargs, "is_satisfied": False}
+
+        game: FFLottery = state.games[(False, "FFLottery", message.author.id)]
+        if game.prev_message is not None:
+            await game.prev_message.delete()
+
+        return kwargs
--- main-app/src/dochi/games/game.py
+++ main-app/src/dochi/games/game.py
@@ -19,11 +19,55 @@ class Game(ABC):
 
 
 class MultiPlayerGame(Game):
-    def __init__(self, client: discord.Client, channel_id: int):
+    def __init__(self, client: discord.Client, channel_id: int, player_id: int, max_num_players: int):
+        assert max_num_players >= 2
+        self.player_ids = [player_id]
+        self.max_num_players = max_num_players
         self.id = (True, self.__class__.__name__, channel_id)
+        self.is_playing = False
+
+    def join(self, player_id: int) -> bool:
+        if player_id in self.player_ids:
+            return False
+
+        if len(self.player_ids) >= self.max_num_players:
+            return False
+
+        self.player_ids.append(player_id)
+        return True
+
+    def start(self) -> bool:
+        if self.is_playing:
+            return False
+
+        self.is_playing = True
+        return True
+
+    def leave(self, player_id: int) -> bool:
+        if player_id in self.player_ids:
+            self.player_ids.remove(player_id)
+            return True
+
+        return False
+
+    def terminate(self) -> bool:
+        if self.is_playing:
+            self.is_playing = False
+            return True
+
+        return False
+
+    @property
+    def is_finished(self) -> bool:
+        # when it is finished, it should be removed
+        return not self.is_playing
 
 
 class SinglePlayerGame(Game):
     def __init__(self, client: discord.Client, user_id: int):
         self.id = (False, self.__class__.__name__, user_id)
+
+    def is_finished(self) -> bool:
+        # when it is finished, it should be removed
+        return False
 
