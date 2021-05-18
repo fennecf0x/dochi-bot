2a0022ce26bbe92b8d7edd2426a926e4f683c3fc
Keonwoo Kim <keonwoo@kaist.ac.kr>
2021-05-19 03:02:26 +0900

    change hash

--- main-app/src/dochi/command/item/sample_from.py
+++ main-app/src/dochi/command/item/sample_from.py
@@ -41,7 +41,7 @@ class SampleFrom(CommandItem, Generic[T]):
 
         for choice in choices:
             salt = "@@DOCHIDOCHI_BOTBOT@@"
-            random.seed(hash(choice + salt + str(state.mood)))
+            random.seed(f"{hash(salt + str(state.mood))}|{choice}|{salt}|KANU")
             priority = random.random()
             prioritised_choices.append((priority, choice))
 
